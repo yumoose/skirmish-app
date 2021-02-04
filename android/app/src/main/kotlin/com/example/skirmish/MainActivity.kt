ecdb1069ff17492855a21bb944f7a611f93676a6
Michael Wilson <1987wilsonm.mw@gmail.com>
2021-02-05 10:07:21 +1300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/skirmish/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.skirmish
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/new_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Page"),
+      ),
+    );
+  }
+}
