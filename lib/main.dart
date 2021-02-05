89a28f1870c838dba824546ac813b41eb46b1976
Jordane Lew <jordane.lew@xero.com>
2021-02-05 15:35:14 +1300

    Add Firebase services and configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:beamer/beamer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'config/service_configuration.dart';
 import 'screens/landing_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   ServiceConfiguration.registerServices();
 
   runApp(MyApp());
@@ -29,24 +32,39 @@ class MyApp extends StatelessWidget {
     LandingLocation(),
   ];
 
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      routerDelegate: BeamerRouterDelegate(
-        initialLocation: initialLocation,
-        notFoundPage: Scaffold(
-          body: Center(
-            child: Text('Not found'),
-          ),
-        ),
-      ),
-      routeInformationParser: BeamerRouteInformationParser(
-        beamLocations: beamLocations,
-      ),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          throw snapshot.error;
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp.router(
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            debugShowCheckedModeBanner: false,
+            routerDelegate: BeamerRouterDelegate(
+              initialLocation: initialLocation,
+              notFoundPage: Scaffold(
+                body: Center(
+                  child: Text('Not found'),
+                ),
+              ),
+            ),
+            routeInformationParser: BeamerRouteInformationParser(
+              beamLocations: beamLocations,
+            ),
+          );
+        }
+
+        return CircularProgressIndicator();
+      },
     );
   }
 }
