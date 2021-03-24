405a2c44be34458d94bc567c3b77e6c27ff1e06c
Jordane Lew <jordane.lew@gmail.com>
2021-03-24 08:39:17 +0000

    Set the app up for l10n and i18n

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localisations.dart';
 import 'package:skirmish/config/themes.dart';
 import 'package:skirmish/config/locations.dart';
 
@@ -7,6 +8,10 @@ class SkirmishApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      onGenerateTitle: (context) =>
+          AppLocalizations.of(context)?.applicationTitle ?? 'Skirmish',
       theme: SkirmishTheme.light,
       darkTheme: SkirmishTheme.dark,
       debugShowCheckedModeBanner: false,
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/intl_browser.dart';
 
 import 'app.dart';
 import 'config/service_configuration.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   ServiceConfiguration.registerServices();
+  await findSystemLocale();
 
   runApp(MyApp());
 }
@@ -17,9 +20,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Intl.defaultLocale = 'en_NZ';
+
     return FutureBuilder(
       future: _initialization,
       builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
+
         if (snapshot.hasError) {
           throw snapshot.error!;
         }
