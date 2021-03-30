064716ac87025480ae6a74d3b0c481a92d44a99e
Jordane Lew <jordane.lew@gmail.com>
2021-03-30 07:21:31 +0000

    Fix tests by adding a rating to memberships under test

--- test/models/membership_test.dart
+++ test/models/membership_test.dart
@@ -7,32 +7,39 @@ import '../mocks/league_mocks.dart.dart';
 import '../mocks/player_mocks.dart';
 
 void main() {
+  final membershipId = 'membership-id';
+  final rating = 1000;
+
+  final playerId = 'player-id';
+  final leagueId = 'league-id';
+
   final membership = Membership.fromSnapshot(
     id: 'membership-id',
     snapshot: {
-      'name': 'membership-name',
-      'player_id': 'player_id',
+      'player_id': playerId,
       'player_snapshot': PlayerMocks.basicDocument,
-      'league_id': 'league_id',
+      'league_id': leagueId,
       'league_snapshot': LeagueMocks.basicDocument,
+      'rating': rating,
     },
   );
 
   group('fromSnapshot', () {
     test('maps fields correctly', () async {
-      expect(membership.id, equals('membership-id'));
+      expect(membership.id, equals(membershipId));
+      expect(membership.rating, equals(rating));
 
       expect(
         membership.player,
         equals(Player.fromSnapshot(
-          id: 'player_id',
+          id: playerId,
           snapshot: PlayerMocks.basicDocument,
         )),
       );
       expect(
         membership.league,
         equals(League.fromSnapshot(
-          id: 'league_id',
+          id: leagueId,
           snapshot: LeagueMocks.basicDocument,
         )),
       );
